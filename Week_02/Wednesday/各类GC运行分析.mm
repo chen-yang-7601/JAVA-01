be18a85a104813cfdaa33ca7b7c5cdb833c87754
YinJennifer <Jennifer_Yin@email.lingan.com.cn>
2021-01-22 21:31:31 +0800

    test mm file

---
+++ Week_02/Wednesday/各类GC运行分析.mm
@@ -0,0 +1,11 @@
+
+<map>
+  <node ID="root" TEXT="各类GC运行分析">
+    <node TEXT="前言：  " ID="2ef6f1f3454fba0cff1d3cb59529500f" STYLE="bubble" POSITION="right"/>
+    <node TEXT="1. 测试程序说明：     " ID="f4a53de74f86111decd39a2899faaa1c" STYLE="bubble" POSITION="right"/>
+    <node TEXT="       通过对一个java程序GCLogAnalysis，分析其在不同的GC策略下的运行情况。这个程序在设定的时间内（默认为一秒），随机地创建100K以内的(byte/int/double类型）的随机对象，执行效率以时间范围内生成的对象数量来衡量。" ID="38e3b21ee7a20ad2a31f9a5f34827318" STYLE="bubble" POSITION="right"/>
+    <node TEXT="2. 环境说明：    " ID="9811e9658772505a57f16f9e617ad3e9" STYLE="bubble" POSITION="right"/>
+    <node TEXT=" - 机器1： " ID="cd86eb35df2e3dca26d59186c98d5e0d" STYLE="bubble" POSITION="right"/>
+    <node TEXT="   16G内存，i7-1165G7cpu, WIN10, jdk1.8.0_221-b11," ID="b50330524d749b5cce5604d04ebc0167" STYLE="bubble" POSITION="right"/>
+  </node>
+</map>
